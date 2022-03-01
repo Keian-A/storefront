e702fdcfc703ddf8ab38526d1adba3fe599eb979
Keian Anthony <KeianAnthony1@gmail.com>
2022-03-01 15:24:51 -0800

    adds day 2 content

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import categoryReducer from './categories.js';
 import productReducer from './products.js';
+import cartReducer from './cart.js';
 
 const reducers = combineReducers({
     categories: categoryReducer,
     products: productReducer,
+    cart: cartReducer,
 });
 
 const createReduxStore = () => {
